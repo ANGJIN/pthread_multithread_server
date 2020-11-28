da523a0a8c042e96dc182b31e3af96e9353add9f
김영진 <zmfjrxmfjr@naver.com>
2020-11-28 18:17:11 +0900

    server 구현 완료

--- server.c
+++ server.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <netinet/in.h>
-#include <netinet/ip.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,13 +12,49 @@
 
 #define MAX_POOL_SIZE 1024
 
-void *service_dispatch(int tid);
+void *worker_job(int tid);
 
-char *httpd(char *command);
+void httpd(char *command, int socket);
 
-pthread_cond_t *cond;
-int *busy;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct myNode *npt;
+typedef struct myNode {
+    int val;
+    npt next;
+} node;
+
+npt head = NULL, tail = NULL;
+
+int isEmpty() {
+    if (head == NULL) {
+        return 1;
+    }
+    return 0;
+}
+
+void enqueue(int socket) {
+    npt new = (npt) malloc(sizeof(node));
+    new->val = socket;
+    new->next = NULL;
+    if (isEmpty()) {
+        head = tail = new;
+    } else {
+        tail->next = new;
+    }
+}
+
+int dequeue() {
+    if (isEmpty())
+        return -1;
+
+    npt top = head;
+    int val = top->val;
+    head = top->next;
+    free(top);
+    return val;
+}
 
 int main(int argc, char **argv) {
 
@@ -69,13 +103,10 @@ int main(int argc, char **argv) {
     pthread_t service_thr;
 
     pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond, NULL);
 
-    cond = (pthread_cond_t *) malloc(sizeof(pthread_cond_t) * pool_size);
-    busy = (int *) calloc(sizeof(int), pool_size);
     for (i = 0; i < pool_size; i++) {
-        pthread_cond_init(&cond[i], NULL);
-
-        if (pthread_create(&service_thr, NULL, service_dispatch, i)) {
+        if (pthread_create(&service_thr, NULL, worker_job, i)) {
             printf("ERROR: error while creating thread\n");
             return -1;
         }
@@ -93,77 +124,51 @@ int main(int argc, char **argv) {
             printf("connection to client failed\n");
             continue;
         } else {
-            // wake up thread in thread pool
-            // #TODO : request queue
-            int worker_idx = -1;
-            for (i = 0; i < pool_size; i++) {
-//                pthread_mutex_lock(&mutex);
-                if (busy[i] == 0) {
-                    worker_idx = i;
-                    break;
-                }
-//                pthread_mutex_unlock(&mutex);
-            }
-            if (worker_idx < 0) {
-                printf("ERROR: socket is full\n");
-                close(client_socket);
-            } else {
-                pthread_mutex_lock(&mutex);
-                busy[worker_idx] = client_socket;
-                printf("wake up thread %d\n", worker_idx);
-                pthread_cond_signal(&cond[worker_idx]);
-                pthread_mutex_unlock(&mutex);
-            }
+            enqueue(client_socket);
+            pthread_cond_signal(&cond);
         }
-
     }
-
     pthread_join(service_thr, NULL);
     return 0;
 }
 
 
-void *service_dispatch(int tid) {
+void *worker_job(int tid) {
     char command[10000];
     printf("create thread %d\n", tid);
     int client_socket;
     while (1) {
         pthread_mutex_lock(&mutex);
-        while (busy[tid] == 0) {
+        while (isEmpty()) {
             printf("thread %d wait\n", tid);
-            pthread_cond_wait(&cond[tid], &mutex);
+            pthread_cond_wait(&cond, &mutex);
+            printf("thread %d up!\n", tid);
+            client_socket = dequeue();
+            if (client_socket > 0)
+                break;
         }
-        printf("thread %d up!\n", tid);
-        client_socket = busy[tid];
         pthread_mutex_unlock(&mutex);
         memset(command, 0, 10000);
 
         read(client_socket, command, 10000);
         printf("client req : %s\n", command);
-        char *buf;
-        buf = httpd(command);
-        write(client_socket, &buf, 100000);
-        close(client_socket);
+        httpd(command, client_socket);
 
+        close(client_socket);
         printf("thread %d finish\n", tid);
-        pthread_mutex_lock(&mutex);
-        busy[tid] = 0;
-        pthread_mutex_unlock(&mutex);
     }
 }
 
-#define SERVER_NAME "micro_httpd"
-#define SERVER_URL "http://www.acme.com/software/micro_httpd/"
+#define SERVER_NAME "ANGJIN"
+#define SERVER_URL "http://www.github.com/angjin"
 #define PROTOCOL "HTTP/1.0"
 #define RFC1123FMT "%a, %d %b %Y %H:%M:%S GMT"
 
 
-/* Forwards. */
-static void file_details(char *dir, char *name);
+static void send_error(int status, char *title, char *extra_header, char *text, int socket);
 
-static void send_error(int status, char *title, char *extra_header, char *text);
-
-static void send_headers(int status, char *title, char *extra_header, char *mime_type, off_t length, time_t mod);
+static void
+send_headers(int status, char *title, char *extra_header, char *mime_type, off_t length, time_t mod, int socket);
 
 static char *get_mime_type(char *name);
 
@@ -171,117 +176,84 @@ static void strdecode(char *to, char *from);
 
 static int hexit(char c);
 
-static void strencode(char *to, size_t tosize, const char *from);
 
+#define PATH_DEFAULT "/var/tmp/20151528/"
 
-char *
-httpd(char *command) {
-    char method[10000], path[10000], protocol[10000], idx[20000], location[20000];
-    char *line, *result;
+void
+httpd(char *command, int socket) {
+    char method[10000], path[10000], protocol[10000];
+    char tmp_path[10000] = PATH_DEFAULT;
+    char *line;
     char *file;
     size_t len;
     int ich;
     struct stat sb;
     FILE *fp;
-    struct dirent **dl;
-    int i, n;
 
-    result = (char *) malloc(100000);
     line = command;
 
-    if (sscanf(line, "%[^ ] %[^ ] %[^ ]", method, path, protocol) != 3)
-        send_error(400, "Bad Request", (char *) 0, "Can't parse request.");
+    if (sscanf(line, "%[^ ] %[^ ] %[^ ]", method, path, protocol) != 3) {
+        send_error(400, "Bad Request", (char *) 0, "Can't parse request.", socket);
+        return;
+    }
 
-    if (strcasecmp(method, "get") != 0)
-        send_error(501, "Not Implemented", (char *) 0, "That method is not implemented.");
-    if (path[0] != '/')
-        send_error(400, "Bad Request", (char *) 0, "Bad filename.");
+    if (strcasecmp(method, "get") != 0) {
+        send_error(501, "Not Implemented", (char *) 0, "That method is not implemented.", socket);
+        return;
+    }
+
+    if (path[0] != '/') {
+        send_error(400, "Bad Request", (char *) 0, "Bad filename.", socket);
+        return;
+    }
+
     file = &(path[1]);
     strdecode(file, file);
-    if (file[0] == '\0')
+    if (file[0] == '\0') {
         file = "./";
+    } else {
+        strcat(tmp_path, file);
+        strcpy(path, tmp_path);
+        file = path + strlen(PATH_DEFAULT);
+    }
+    printf("path : %s\nfile : %s\n", path, file);
+
     len = strlen(file);
     if (file[0] == '/' || strcmp(file, "..") == 0 || strncmp(file, "../", 3) == 0 ||
-        strstr(file, "/../") != (char *) 0 || strcmp(&(file[len - 3]), "/..") == 0)
-        send_error(400, "Bad Request", (char *) 0, "Illegal filename.");
-    if (stat(file, &sb) < 0)
-        send_error(404, "Not Found", (char *) 0, "File not found.");
-
-    else if (S_ISDIR(sb.st_mode)) {
-        if (file[len - 1] != '/') {
-            (void) snprintf(
-                    location, sizeof(location), "Location: %s/", path);
-            send_error(302, "Found", location, "Directories must end with a slash.");
-        }
-        (void) snprintf(idx, sizeof(idx), "%sindex.html", file);
-        if (stat(idx, &sb) >= 0) {
-            file = idx;
-            goto do_file;
-        }
-        send_headers(200, "Ok", (char *) 0, "text/html", -1, sb.st_mtime);
-        (void) printf("\
-<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n\
-<html>\n\
-  <head>\n\
-    <meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\">\n\
-    <title>Index of %s</title>\n\
-  </head>\n\
-  <body bgcolor=\"#99cc99\">\n\
-    <h4>Index of %s</h4>\n\
-    <pre>\n", file, file);
-        n = scandir(file, &dl, NULL, alphasort);
-        if (n < 0) {
-            perror("scandir");
-        } else {
-            for (i = 0; i < n; ++i)
-                file_details(file, dl[i]->d_name);
-        }
-        (void) printf("\
-    </pre>\n\
-    <hr>\n\
-    <address><a href=\"%s\">%s</a></address>\n\
-  </body>\n\
-</html>\n", SERVER_URL, SERVER_NAME);
-    } else {
-        do_file:
-        fp = fopen(file, "r");
-        if (fp == (FILE *) 0) {
-            send_error(403, "Forbidden", (char *) 0, "File is protected.");
-        } else {
-            send_headers(200, "Ok", (char *) 0, get_mime_type(file), sb.st_size, sb.st_mtime);
-            while ((ich = getc(fp)) != EOF)
-                putchar(ich);
-        }
+        strstr(file, "/../") != (char *) 0 || strcmp(&(file[len - 3]), "/..") == 0) {
+        send_error(400, "Bad Request", (char *) 0, "Illegal filename.", socket);
+        return;
     }
-    (void) fflush(stdout);
-    return result;
-}
 
+    if (stat(path, &sb) < 0) {
+        send_error(404, "Not Found", (char *) 0, "File not found.", socket);
+        return;
+    }
 
-static void
-file_details(char *dir, char *name) {
-    static char encoded_name[1000];
-    static char path[2000];
-    struct stat sb;
-    char timestr[16];
+    if (S_ISDIR(sb.st_mode)) {
+        send_error(400, "Bad Request", (char *) 0, "Directory is not allowed.", socket);
+        return;
+    }
 
-    strencode(encoded_name, sizeof(encoded_name), name);
-    (void) snprintf(path, sizeof(path), "%s/%s", dir, name);
-    if (lstat(path, &sb) < 0)
-        (void) printf("<a href=\"%s\">%-32.32s</a>    ???\n", encoded_name, name);
-    else {
-        (void) strftime(timestr, sizeof(timestr), "%d%b%Y %H:%M", localtime(&sb.st_mtime));
-        (void) printf("<a href=\"%s\">%-32.32s</a>    %15s %14lld\n", encoded_name, name, timestr,
-                      (long long) sb.st_size);
+    // send file
+    fp = fopen(path, "r");
+    if (fp == (FILE *) 0) {
+        send_error(403, "Forbidden", (char *) 0, "File is protected.", socket);
+        return;
+    }
+
+    send_headers(200, "Ok", (char *) 0, get_mime_type(file), sb.st_size, sb.st_mtime, socket);
+    while ((ich = getc(fp)) != EOF) {
+        write(socket, &ich, 1);
     }
 }
 
-
 static void
-send_error(int status, char *title, char *extra_header, char *text) {
-    send_headers(status, title, extra_header, "text/html", -1, -1);
-    (void) printf("\
-<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n\
+send_error(int status, char *title, char *extra_header, char *text, int socket) {
+    send_headers(status, title, extra_header, "text/html", -1, -1, socket);
+    char buf[10000];
+    (void) sprintf(buf, "\
+<!DOCTYPE html>\n\
 <html>\n\
   <head>\n\
     <meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\">\n\
@@ -289,38 +261,52 @@ send_error(int status, char *title, char *extra_header, char *text) {
   </head>\n\
   <body bgcolor=\"#cc9999\">\n\
     <h4>%d %s</h4>\n", status, title, status, title);
-    (void) printf("%s\n", text);
-    (void) printf("\
+    write(socket, buf, strlen(buf));
+    (void) sprintf(buf, "%s\n", text);
+    write(socket, buf, strlen(buf));
+    (void) sprintf(buf, "\
     <hr>\n\
     <address><a href=\"%s\">%s</a></address>\n\
   </body>\n\
 </html>\n", SERVER_URL, SERVER_NAME);
+    write(socket, buf, strlen(buf));
     (void) fflush(stdout);
 }
 
-
 static void
-send_headers(int status, char *title, char *extra_header, char *mime_type, off_t length, time_t mod) {
+send_headers(int status, char *title, char *extra_header, char *mime_type, off_t length, time_t mod, int socket) {
     time_t now;
-    char timebuf[100];
+    char timebuf[100], buf[1000];
 
-    (void) printf("%s %d %s\015\012", PROTOCOL, status, title);
-    (void) printf("Server: %s\015\012", SERVER_NAME);
+    (void) sprintf(buf, "%s %d %s\015\012", PROTOCOL, status, title);
+    write(socket, buf, strlen(buf));
+    (void) sprintf(buf, "Server: %s\015\012", SERVER_NAME);
+    write(socket, buf, strlen(buf));
     now = time((time_t *) 0);
     (void) strftime(timebuf, sizeof(timebuf), RFC1123FMT, gmtime(&now));
-    (void) printf("Date: %s\015\012", timebuf);
-    if (extra_header != (char *) 0)
-        (void) printf("%s\015\012", extra_header);
-    if (mime_type != (char *) 0)
-        (void) printf("Content-Type: %s\015\012", mime_type);
-    if (length >= 0)
-        (void) printf("Content-Length: %lld\015\012", (long long) length);
+    (void) sprintf(buf, "Date: %s\015\012", timebuf);
+    write(socket, buf, strlen(buf));
+    if (extra_header != (char *) 0) {
+        (void) sprintf(buf, "%s\015\012", extra_header);
+        write(socket, buf, strlen(buf));
+    }
+    if (mime_type != (char *) 0) {
+        (void) sprintf(buf, "Content-Type: %s\015\012", mime_type);
+        write(socket, buf, strlen(buf));
+    }
+    if (length >= 0) {
+        (void) sprintf(buf, "Content-Length: %lld\015\012", (long long) length);
+        write(socket, buf, strlen(buf));
+    }
     if (mod != (time_t) -1) {
         (void) strftime(timebuf, sizeof(timebuf), RFC1123FMT, gmtime(&mod));
-        (void) printf("Last-Modified: %s\015\012", timebuf);
+        (void) sprintf(buf, "Last-Modified: %s\015\012", timebuf);
+        write(socket, buf, strlen(buf));
     }
-    (void) printf("Connection: close\015\012");
-    (void) printf("\015\012");
+    (void) sprintf(buf, "Connection: close\015\012");
+    write(socket, buf, strlen(buf));
+    (void) sprintf(buf, "\015\012");
+    write(socket, buf, strlen(buf));
 }
 
 
@@ -391,23 +377,4 @@ hexit(char c) {
     if (c >= 'A' && c <= 'F')
         return c - 'A' + 10;
     return 0;        /* shouldn't happen, we're guarded by isxdigit() */
-}
-
-
-static void
-strencode(char *to, size_t tosize, const char *from) {
-    int tolen;
-
-    for (tolen = 0; *from != '\0' && tolen + 4 < tosize; ++from) {
-        if (isalnum(*from) || strchr("/_.-~", *from) != (char *) 0) {
-            *to = *from;
-            ++to;
-            ++tolen;
-        } else {
-            (void) sprintf(to, "%%%02x", (int) *from & 0xff);
-            to += 3;
-            tolen += 3;
-        }
-    }
-    *to = '\0';
 }
